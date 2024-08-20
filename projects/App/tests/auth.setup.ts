703453f3c323765185d7f4fc9e818c397ee3ce85
mai-expedock <imma.damalerio@expedock.com>
2024-08-20 10:02:44 +0800

    Remove folder

--- projects/App/tests/auth.setup.ts
+++ projects/App/tests/auth.setup.ts
@@ -7,8 +7,8 @@ const GLOBALTIMEOUT = 60000
 setup('authenticate', async ({ page, browser }) => {
   await page.goto('https://passive-app.expedock.com/');
   //Change this to variable
- 
-  
+  await page.locator("#username").fill("qa-passive-1@expedock.com")
+  await page.locator("#password").fill("GgZHcs73:A,6~KM]{f%tz_")
   await page.getByRole('button', { name: 'Continue', exact: true }).click()
   await page.waitForURL('https://passive-app.expedock.com/');
   await expect(page.getByRole('button', { name: '+ Create Task' })).toBeVisible({ timeout: GLOBALTIMEOUT });
