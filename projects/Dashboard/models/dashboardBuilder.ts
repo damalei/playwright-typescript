59f0cd0723dfe1d55153dcf3cdd68fb1caf507bc
mai-expedock <imma.damalerio@expedock.com>
2024-12-03 14:24:25 +1100

    Format document and removed comments

--- projects/Dashboard/models/dashboardBuilder.ts
+++ projects/Dashboard/models/dashboardBuilder.ts
@@ -5,37 +5,29 @@ import { waitForFilterSectionToLoad } from "../../utils";
 const GLOBALTIMEOUT = 60000;
 const DEFAULT_GLOBAL_TIMEOUT_MS = GLOBALTIMEOUT;
 
-export class DashboardBuilder{
-    readonly page: Page
-    readonly GlobalFilterSection: GlobalFilterSection
-    readonly searchDashboard: Locator
-    readonly snackBar: Locator
+export class DashboardBuilder {
+  readonly page: Page;
+  readonly GlobalFilterSection: GlobalFilterSection;
+  readonly searchDashboard: Locator;
+  readonly snackBar: Locator;
 
-constructor(page: Page){
-    this.page = page
-    this.GlobalFilterSection = new GlobalFilterSection(page)
-    this.searchDashboard = page.getByPlaceholder("Search dashboards")
-    }
+  constructor(page: Page) {
+    this.page = page;
+    this.GlobalFilterSection = new GlobalFilterSection(page);
+    this.searchDashboard = page.getByPlaceholder("Search dashboards");
+  }
 
-async loadDashboard(dashboard: string) {
-    this.searchDashboard.fill(dashboard)
-    this.page.getByTestId(`dashboard-builder-tab-${dashboard}`).click()
-    await waitForFilterSectionToLoad(this.page, DEFAULT_GLOBAL_TIMEOUT_MS)
-    }
+  async loadDashboard(dashboard: string) {
+    this.searchDashboard.fill(dashboard);
+    this.page.getByTestId(`dashboard-builder-tab-${dashboard}`).click();
+    await waitForFilterSectionToLoad(this.page, DEFAULT_GLOBAL_TIMEOUT_MS);
+  }
 
-async exitAndReturnDashboard(dashboard1: string, dashboard2: string){
-    this.page.goto(dashboard1)
-    await waitForFilterSectionToLoad(this.page, DEFAULT_GLOBAL_TIMEOUT_MS)
-    this.page.goto(dashboard2)
-    await waitForFilterSectionToLoad(this.page, DEFAULT_GLOBAL_TIMEOUT_MS)
-    }
-
-// async waitForSnackBars(){
-//     // const snack = this.page.locator('//div[@id="notistack-snackbar"]')
-//     // snack.waitFor({
-//     //     state: 'detached',
-//     //     timeout: 10000
-//     //     })
-// }
+  async exitAndReturnDashboard(dashboard1: string, dashboard2: string) {
+    this.page.goto(dashboard1);
+    await waitForFilterSectionToLoad(this.page, DEFAULT_GLOBAL_TIMEOUT_MS);
+    this.page.goto(dashboard2);
+    await waitForFilterSectionToLoad(this.page, DEFAULT_GLOBAL_TIMEOUT_MS);
+  }
 
 }
--- projects/Dashboard/models/sideMenu.ts
+++ projects/Dashboard/models/sideMenu.ts
@@ -1,18 +1,15 @@
 import { Locator, Page } from "@playwright/test";
-// import { GlobalFilterSection } from "./globalFilterSection";
 
 export class SideMenu {
-    readonly page: Page
-    // readonly globalFilterSection: GlobalFilterSection
-    readonly userProfile: Locator
-    readonly dashboardBuilderOption: Locator
+  readonly page: Page;
+  readonly userProfile: Locator;
+  readonly dashboardBuilderOption: Locator;
 
-constructor(page: Page){
+  constructor(page: Page) {
     this.page = page;
-    // this.globalFilterSection = new GlobalFilterSection(page)
-    this.userProfile = page.getByTestId("account-user-name")
-    this.dashboardBuilderOption = page.locator('span').filter({ hasText: "Dashboard Builder" })}
-
-
-
+    this.userProfile = page.getByTestId("account-user-name");
+    this.dashboardBuilderOption = page
+      .locator("span")
+      .filter({ hasText: "Dashboard Builder" });
+  }
 }
