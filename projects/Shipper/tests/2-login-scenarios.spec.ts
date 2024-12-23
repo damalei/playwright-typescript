5550746f19d84f6994c8a2a8a9330e072c3d533c
mai-expedock <imma.damalerio@expedock.com>
2024-12-23 15:21:26 +1100

    added TC numbers

--- projects/Shipper/tests/2-login-scenarios.spec.ts
+++ projects/Shipper/tests/2-login-scenarios.spec.ts
@@ -18,8 +18,8 @@ test.describe('Login', () => {
   });
 
   test('[30.1] Log-in using correct user credentials', async ({ page }) => {
-    await page.getByLabel('Email Address').fill(username);
-    await page.getByLabel('Password').fill(password);
+    await page.getByLabel('Email Address').fill(`${process.env.SHIPPER_VIZ_VALID_USER}`);
+    await page.getByLabel('Password').fill(`${process.env.SHIPPER_VIZ_VALID_PASS}`);
     await page.getByRole('button', { name: 'LOG IN' }).click();
     await expect
       .soft(page.getByTestId('exceptions-management-header'))
@@ -27,8 +27,8 @@ test.describe('Login', () => {
   });
 
   test('[30.2] Log-in using incorrect username', async ({ page }) => {
-    await page.getByLabel('Email Address').fill(wrongusername);
-    await page.getByLabel('Password').fill(password);
+    await page.getByLabel('Email Address').fill(`${process.env.SHIPPER_VIZ_INVALID_USER}`);
+    await page.getByLabel('Password').fill(`${process.env.SHIPPER_VIZ_VALID_PASS}`);
     await page.getByRole('button', { name: 'LOG IN' }).click();
     await expect
       .soft(page.getByText('Wrong email or password'))
@@ -36,8 +36,8 @@ test.describe('Login', () => {
   });
 
   test('[30.3] Log-in using incorrect password', async ({ page }) => {
-    await page.getByLabel('Email Address').fill(username);
-    await page.getByLabel('Password').fill(wrongpassword);
+    await page.getByLabel('Email Address').fill(`${process.env.SHIPPER_VIZ_VALID_USER}`);
+    await page.getByLabel('Password').fill(`${process.env.SHIPPER_VIZ_INVALID_PASS}`);
     await page.getByRole('button', { name: 'LOG IN' }).click();
     await expect
       .soft(page.getByText('Wrong email or password'))
@@ -46,13 +46,13 @@ test.describe('Login', () => {
 
   test('[30.4] Log-in when username is empty', async ({ page }) => {
     await page.getByLabel('Email Address').fill('');
-    await page.getByLabel('Password').fill(password);
+    await page.getByLabel('Password').fill(`${process.env.SHIPPER_VIZ_VALID_PASS}`);
     const loginBtn = page.getByRole('button', { name: 'LOG IN' });
     await expect.soft(loginBtn).toBeDisabled();
   });
 
   test('[30.5] Log-in when psw is empty', async ({ page }) => {
-    await page.getByLabel('Email Address').fill(username);
+    await page.getByLabel('Email Address').fill(`${process.env.SHIPPER_VIZ_VALID_USER}`);
     await page.getByLabel('Password').fill('');
     const loginBtn = page.getByRole('button', { name: 'LOG IN' });
     await expect.soft(loginBtn).toBeDisabled();
@@ -69,7 +69,7 @@ test.describe('Login', () => {
 
   test('[30.7] Forgot password', async ({ page }) => {
     await page.getByRole('link', { name: 'Forgot Password' }).click();
-    await page.getByLabel('Email Address').fill(username);
+    await page.getByLabel('Email Address').fill(`${process.env.SHIPPER_VIZ_VALID_USER}`);
     await page.getByRole('button', { name: 'SUBMIT' }).click();
     await expect
       .soft(page.getByText('Successfully sent!'))
@@ -77,8 +77,8 @@ test.describe('Login', () => {
   });
 
   test('[30.8] Log out', async ({ page }) => {
-    await page.getByLabel('Email Address').fill(username);
-    await page.getByLabel('Password').fill(password);
+    await page.getByLabel('Email Address').fill(`${process.env.SHIPPER_VIZ_VALID_USER}`);
+    await page.getByLabel('Password').fill(`${process.env.SHIPPER_VIZ_VALID_PASS}`);
     await page.getByRole('button', { name: 'LOG IN' }).click();
     await expect
       .soft(page.getByTestId('exceptions-management-header'))
