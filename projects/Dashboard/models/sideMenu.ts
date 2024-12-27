8bbda127f4b1d7920df77748256ba777209d4721
mai-expedock <imma.damalerio@expedock.com>
2024-12-27 22:17:46 +1100

    changes for edit side menu dashboard list

--- projects/Dashboard/models/sideMenu.ts
+++ projects/Dashboard/models/sideMenu.ts
@@ -4,6 +4,9 @@ export class SideMenu {
   readonly page: Page;
   readonly userProfile: Locator;
   readonly dashboardBuilderOption: Locator;
+  readonly listUserManagement: Locator
+  readonly accBP: Locator;
+  readonly listWrapperBusiness: Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -11,5 +14,9 @@ export class SideMenu {
     this.dashboardBuilderOption = page
       .locator('span')
       .filter({ hasText: 'Dashboard Builder' });
+    this.listUserManagement = page.locator('//span[text()="User Management"]')
+    this.accBP = page.locator('//span[text()="Business Performance"]')
+    this.listWrapperBusiness = page.getByTestId('sidebar-tab-wrapper-BUSINESS_PERFORMANCE')
   }
+  
 }
