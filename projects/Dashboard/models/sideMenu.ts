a4ca889d32794cdf5bc75f7d6aee55fd245b756b
mai-expedock <imma.damalerio@expedock.com>
2024-12-27 13:40:39 +1100

    removed files that is not part of feature test change

--- projects/Dashboard/models/sideMenu.ts
+++ projects/Dashboard/models/sideMenu.ts
@@ -4,16 +4,12 @@ export class SideMenu {
   readonly page: Page;
   readonly userProfile: Locator;
   readonly dashboardBuilderOption: Locator;
-  readonly userManagement: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.userProfile = page.getByTestId('account-user-name');
     this.dashboardBuilderOption = page
-      .locator("span")
-      .filter({ hasText: "Dashboard Builder" });
-      this.userManagement = page
-      .locator("span")
-      .filter({ hasText: "User Management" });
+      .locator('span')
+      .filter({ hasText: 'Dashboard Builder' });
   }
 }
