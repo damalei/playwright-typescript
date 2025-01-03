d1146025ff80bf56334f7cee57a2ea16e5dcd117
mai-expedock <imma.damalerio@expedock.com>
2025-01-03 16:24:36 +1100

    changed filename of test spec from 8 to 33, syncing with TS number

---
+++ projects/Dashboard/tests/33-dashboard-edit.spec.ts
@@ -0,0 +1,62 @@
+import { Page, test, expect } from '@playwright/test';
+import { waitForElementToHide } from '../../utils';
+import { DEFAULT_TIMEOUT_IN_MS, FREIGHT_BI_BASE_URL } from '../../constants';
+import { SideMenu } from '../models/sideMenu';
+import { UserManagement } from '../models/userManagement';
+
+const xpathUserManagementButton = '//button[text()="Save"]'
+const dash1 = 'QA Chart Collection'
+const dash2 = 'QA Test Template'
+
+test.describe('[33]User Edit dashboard list on side menu ', () => {
+    let page: Page;
+    test.beforeAll(async ({ browser }) => {
+      page = await browser.newPage();
+      await page.goto('https://passive-dashboard.expedock.com/');
+    });
+
+    test('[33.1] add dashboards', async () => {
+        const side = new SideMenu(page)
+        const user = new UserManagement(page)
+        await page.goto(FREIGHT_BI_BASE_URL)
+        await side.userProfile.click()
+        await side.listUserManagement.click()
+        await page.keyboard.press('Escape')
+        await user.searchEmail(`${process.env.FREIGHT_BI_CLIENT_USER}`)
+        await user.clickEditAccess(`${process.env.FREIGHT_BI_CLIENT_USER}`)
+        await user.inputDashboard('Business Performance', `${dash1}`)
+        await user.inputDashboard('Business Performance', `${dash2}`)
+        await user.buttonSave.click()
+        await waitForElementToHide(page, DEFAULT_TIMEOUT_IN_MS, `${xpathUserManagementButton}`)
+        await page.reload()
+        await side.accBP.click()
+        await expect.soft(page.locator(`//span[text()='${dash1}']`)).toBeVisible()
+        await expect.soft(page.locator(`//span[text()='${dash2}']`)).toBeVisible()
+    })
+
+    test('[33.3] verify dashboard is in the correct order', async () => {
+        const side = new SideMenu(page)
+        const menuList = await side.listWrapperBusiness.locator('li')
+        const expectList = [`${dash1}`, `${dash2}`]
+        await expect(menuList).toContainText(expectList)
+    })
+
+    test('[33.2] remove dashboards', async () => {
+        const side = new SideMenu(page)
+        const user = new UserManagement(page)
+        await page.goto(FREIGHT_BI_BASE_URL)
+        await side.userProfile.click()
+        await side.listUserManagement.click()
+        await page.keyboard.press('Escape')
+        await user.searchEmail(`${process.env.FREIGHT_BI_CLIENT_USER}`)
+        await user.clickEditAccess(`${process.env.FREIGHT_BI_CLIENT_USER}`)
+        await user.inputDashboard('Business Performance', `${dash1}`)
+        await user.inputDashboard('Business Performance', `${dash2}`)
+        await user.buttonSave.click()
+        await waitForElementToHide(page, DEFAULT_TIMEOUT_IN_MS, `${xpathUserManagementButton}`)
+        await page.reload()
+        await side.accBP.click()
+        await expect.soft(page.locator(`//span[text()='${dash1}']`)).toBeHidden()
+        await expect.soft(page.locator(`//span[text()='${dash2}']`)).toBeHidden()
+    })
+})
