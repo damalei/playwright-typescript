cd845f08bf730a35fb567c1838261dc6c0bcd589
mai-expedock <imma.damalerio@expedock.com>
2025-01-06 10:46:57 +1100

    format changes

--- projects/Dashboard/tests/11-dashboardbuilder-selectors.spec.ts
+++ projects/Dashboard/tests/11-dashboardbuilder-selectors.spec.ts
@@ -1,5 +1,9 @@
 import { test, Page, expect } from '@playwright/test';
-import { waitForElementToHide, waitForFilterSectionToLoad, waitForSnackBar } from '../../utils';
+import {
+  waitForElementToHide,
+  waitForFilterSectionToLoad,
+  waitForSnackBar,
+} from '../../utils';
 import { SideMenu } from '../models/sideMenu';
 import { DEFAULT_TIMEOUT_IN_MS, FREIGHT_BI_BASE_URL } from '../../constants';
 import { DashboardBuilder } from '../models/dashboardBuilder';
@@ -20,119 +24,146 @@ test.describe('Edit and save filters fields on Dashboard Builder', () => {
     const user = new UserManagement(page);
     const side = new SideMenu(page);
     await page.goto(FREIGHT_BI_BASE_URL);
-    await side.userProfile.click()
-    await side.listUserManagement.click()
-    await page.keyboard.press('Escape')
-    await user.searchEmail(`${process.env.FREIGHT_BI_CLIENT_USER}`)
-    await user.clickEditAccess(`${process.env.FREIGHT_BI_CLIENT_USER}`)
-    await user.inputDashboard('Business Performance', 'QA Test Template')
-    await user.buttonSave.click()
-    await waitForElementToHide(page, DEFAULT_TIMEOUT_IN_MS, '//button[text()="Save"]')
+    await side.userProfile.click();
+    await side.listUserManagement.click();
+    await page.keyboard.press('Escape');
+    await user.searchEmail(`${process.env.FREIGHT_BI_CLIENT_USER}`);
+    await user.clickEditAccess(`${process.env.FREIGHT_BI_CLIENT_USER}`);
+    await user.inputDashboard('Business Performance', 'QA Test Template');
+    await user.buttonSave.click();
+    await waitForElementToHide(
+      page,
+      DEFAULT_TIMEOUT_IN_MS,
+      '//button[text()="Save"]'
+    );
   });
 
   test('[11.1 & 11.3] User adds selector FIELD from the basic view', async () => {
     const dashbuild = new DashboardBuilder(page);
     const user = new UserManagement(page);
     const side = new SideMenu(page);
-    await page.goto(dashboard2) 
+    await page.goto(dashboard2);
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
     await dashbuild.buttonEditDashboard.click();
     await dashbuild.clickSelectorToggle('ORG TYPE');
-    await dashbuild.setSelectorValue('ORG TYPE', 'Shipper')
-    await dashbuild.GlobalFilterSection.saveViewButton.click()
+    await dashbuild.setSelectorValue('ORG TYPE', 'Shipper');
+    await dashbuild.GlobalFilterSection.saveViewButton.click();
     await dashbuild.buttonSave.click();
-    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).toBeVisible() 
-    await expect.soft(page.locator('//input[@value="Shipper"]')).toBeVisible()
+    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).toBeVisible();
+    await expect.soft(page.locator('//input[@value="Shipper"]')).toBeVisible();
   });
 
   test('[11.2] User saves a selector FIELD from the basic view', async () => {
     const dashbuild = new DashboardBuilder(page);
     const user = new UserManagement(page);
     const side = new SideMenu(page);
-    await page.goto(FREIGHT_BI_BASE_URL)
-    await side.userProfile.click()
-    await side.listUserManagement.click()
-    await page.keyboard.press('Escape')
-    await user.searchEmail(`${process.env.FREIGHT_BI_CLIENT_USER}`)
-    await user.clickEditAccess(`${process.env.FREIGHT_BI_CLIENT_USER}`)
-    await user.inputDashboard('Business Performance', 'QA Test Template')
-    await user.buttonSave.click()
-    await waitForElementToHide(page, DEFAULT_TIMEOUT_IN_MS, '//button[text()="Save"]')
-    await page.reload()
-    await side.accBP.click()
-    await side.clickOnDashboardName('QA Test Template')
+    await page.goto(FREIGHT_BI_BASE_URL);
+    await side.userProfile.click();
+    await side.listUserManagement.click();
+    await page.keyboard.press('Escape');
+    await user.searchEmail(`${process.env.FREIGHT_BI_CLIENT_USER}`);
+    await user.clickEditAccess(`${process.env.FREIGHT_BI_CLIENT_USER}`);
+    await user.inputDashboard('Business Performance', 'QA Test Template');
+    await user.buttonSave.click();
+    await waitForElementToHide(
+      page,
+      DEFAULT_TIMEOUT_IN_MS,
+      '//button[text()="Save"]'
+    );
+    await page.reload();
+    await side.accBP.click();
+    await side.clickOnDashboardName('QA Test Template');
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
-    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).toBeVisible()
-    await expect.soft(page.locator('//input[@value="Shipper"]')).toBeVisible()
+    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).toBeVisible();
+    await expect.soft(page.locator('//input[@value="Shipper"]')).toBeVisible();
   });
 
   test('[11.5] User changes a value on the selector field', async () => {
     const dashbuild = new DashboardBuilder(page);
     const user = new UserManagement(page);
     const side = new SideMenu(page);
-    await page.goto(dashboard2) 
+    await page.goto(dashboard2);
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
     await dashbuild.buttonEditDashboard.click();
-    await dashbuild.setSelectorValue('ORG TYPE', 'Creditor')
-    await dashbuild.GlobalFilterSection.saveViewButton.click()
+    await dashbuild.setSelectorValue('ORG TYPE', 'Creditor');
+    await dashbuild.GlobalFilterSection.saveViewButton.click();
     await dashbuild.buttonSave.click();
-    await page.goto(FREIGHT_BI_BASE_URL)
-    await side.accBP.click()
-    await side.clickOnDashboardName('QA Test Template')
+    await page.goto(FREIGHT_BI_BASE_URL);
+    await side.accBP.click();
+    await side.clickOnDashboardName('QA Test Template');
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
-    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).toBeVisible()
-    await expect.soft(page.locator('//input[@value="Creditor"]')).toBeVisible()
+    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).toBeVisible();
+    await expect.soft(page.locator('//input[@value="Creditor"]')).toBeVisible();
   });
 
   test('[11.4] User removes a selector field', async () => {
     const dashbuild = new DashboardBuilder(page);
     const user = new UserManagement(page);
     const side = new SideMenu(page);
-    await page.goto(dashboard2) 
+    await page.goto(dashboard2);
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
     await dashbuild.buttonEditDashboard.click();
     await dashbuild.clickSelectorToggle('ORG TYPE');
-    await dashbuild.GlobalFilterSection.saveViewButton.click()
+    await dashbuild.GlobalFilterSection.saveViewButton.click();
     await dashbuild.buttonSave.click();
-    await page.goto(FREIGHT_BI_BASE_URL)
-    await side.accBP.click()
-    await side.clickOnDashboardName('QA Test Template')
+    await page.goto(FREIGHT_BI_BASE_URL);
+    await side.accBP.click();
+    await side.clickOnDashboardName('QA Test Template');
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
-    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).not.toBeVisible()
+    await expect
+      .soft(page.locator('//h6[text()="ORG TYPE"]'))
+      .not.toBeVisible();
   });
 
-  test.skip('[11.X] User removes all selector fields', {
-        annotation: {
-            type: 'feature with issue',
-            description: 'https://expedock.atlassian.net/browse/PD-10263',
-        },
-    }, async () => {
-    const dashbuild = new DashboardBuilder(page);
-    const user = new UserManagement(page);
-    const side = new SideMenu(page);
-    await page.goto(dashboard2) 
-    await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
-    await dashbuild.buttonEditDashboard.click();
-    await dashbuild.clickSelectorToggle('ORG TYPE');
-    await dashbuild.clickSelectorToggle('PERIOD');
-    await dashbuild.clickSelectorToggle('CURRENCY');
-    await dashbuild.clickSelectorToggle('SHIPMENT VOLUME');
-    await dashbuild.clickSelectorToggle('WEIGHT');
-    await dashbuild.clickSelectorToggle('VOLUME');
-    await dashbuild.clickSelectorToggle('GROUP BY DATE');
-    await dashbuild.GlobalFilterSection.saveViewButton.click()
-    await dashbuild.buttonSave.click();
-    await page.goto(FREIGHT_BI_BASE_URL)
-    await side.accBP.click()
-    await side.clickOnDashboardName('QA Test Template')
-    await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
-    await expect.soft(page.locator('//h6[text()="ORG TYPE"]')).not.toBeVisible()
-    await expect.soft(page.locator('//h6[text()="PERIOD"]')).not.toBeVisible()
-    await expect.soft(page.locator('//h6[text()="CURRENCY"]')).not.toBeVisible()
-    await expect.soft(page.locator('//h6[text()="SHIPMENT VOLUME"]')).not.toBeVisible()
-    await expect.soft(page.locator('//h6[text()="WEIGHT"]')).not.toBeVisible()
-    await expect.soft(page.locator('//h6[text()="VOLUME"]')).not.toBeVisible()
-    await expect.soft(page.locator('//h6[text()="GROUP BY DATE"]')).not.toBeVisible()
-  });
-
+  test.skip(
+    '[11.X] User removes all selector fields',
+    {
+      annotation: {
+        type: 'feature with issue',
+        description: 'https://expedock.atlassian.net/browse/PD-10263',
+      },
+    },
+    async () => {
+      const dashbuild = new DashboardBuilder(page);
+      const user = new UserManagement(page);
+      const side = new SideMenu(page);
+      await page.goto(dashboard2);
+      await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
+      await dashbuild.buttonEditDashboard.click();
+      await dashbuild.clickSelectorToggle('ORG TYPE');
+      await dashbuild.clickSelectorToggle('PERIOD');
+      await dashbuild.clickSelectorToggle('CURRENCY');
+      await dashbuild.clickSelectorToggle('SHIPMENT VOLUME');
+      await dashbuild.clickSelectorToggle('WEIGHT');
+      await dashbuild.clickSelectorToggle('VOLUME');
+      await dashbuild.clickSelectorToggle('GROUP BY DATE');
+      await dashbuild.GlobalFilterSection.saveViewButton.click();
+      await dashbuild.buttonSave.click();
+      await page.goto(FREIGHT_BI_BASE_URL);
+      await side.accBP.click();
+      await side.clickOnDashboardName('QA Test Template');
+      await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
+      await expect
+        .soft(page.locator('//h6[text()="ORG TYPE"]'))
+        .not.toBeVisible();
+      await expect
+        .soft(page.locator('//h6[text()="PERIOD"]'))
+        .not.toBeVisible();
+      await expect
+        .soft(page.locator('//h6[text()="CURRENCY"]'))
+        .not.toBeVisible();
+      await expect
+        .soft(page.locator('//h6[text()="SHIPMENT VOLUME"]'))
+        .not.toBeVisible();
+      await expect
+        .soft(page.locator('//h6[text()="WEIGHT"]'))
+        .not.toBeVisible();
+      await expect
+        .soft(page.locator('//h6[text()="VOLUME"]'))
+        .not.toBeVisible();
+      await expect
+        .soft(page.locator('//h6[text()="GROUP BY DATE"]'))
+        .not.toBeVisible();
+    }
+  );
 });
