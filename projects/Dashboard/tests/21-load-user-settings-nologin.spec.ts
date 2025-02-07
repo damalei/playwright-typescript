100a483eca8155db617cf963f68ead20aa0b9402
mai-expedock <132625237+mai-expedock@users.noreply.github.com>
2025-02-07 13:43:50 +0800

    [PD-10282]  Code Fix (#44)
    * [PD-10283] Toggle on sandboxing
    
    * change in tc numbers
    
    * format changes
    
    * [PD-10282] Code fix
    
    * format changes

--- projects/Dashboard/tests/21-load-user-settings-nologin.spec.ts
+++ projects/Dashboard/tests/21-load-user-settings-nologin.spec.ts
@@ -2,6 +2,7 @@ import { expect, test, Page } from '@playwright/test';
 import { FREIGHT_BI_BASE_URL, DEFAULT_TIMEOUT_IN_MS } from '../../constants';
 import { waitForFilterSectionToLoad, logInAuth } from '../../utils';
 import { ExploreShipments } from '../models/exploreShipments';
+import { ExplorePayableInvoices } from '../models/explorePayableInvoices';
 import { SideMenu } from '../models/sideMenu';
 const { chromium } = require('playwright');
 
@@ -16,11 +17,12 @@ test.describe('[21] Load dashboard and filter settings', () => {
       `${process.env.FREIGHT_BI_CLIENT3_USER}`,
       `${process.env.FREIGHT_BI_CLIENT3_PASS}`
     );
+    const ship = new ExploreShipments(page);
+    await ship.goto();
   });
 
   test('[21.3] User loads a dashboard with filter and selector settings customized at the user level', async () => {
     const ship = new ExploreShipments(page);
-    await ship.goto();
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
     const weight = await ship.globalFilterSection.checkSelector('WEIGHT', 'LB');
     const volume = await ship.globalFilterSection.checkSelector('VOLUME', 'CC');
@@ -32,31 +34,32 @@ test.describe('[21] Load dashboard and filter settings', () => {
       'AP Is Posted',
       'True'
     );
-    await ship.globalFilterSection.fieldTransportMode.click();
-    const multText1 = await ship.globalFilterSection.checkFilterFieldChip(
+    const seaChip = await ship.globalFilterSection.checkFilterFieldChip(
       'Transport Mode',
       'SEA'
     );
-    const multText2 = await ship.globalFilterSection.checkFilterFieldChip(
+    await page.waitForTimeout(5000);
+    const airChip = await ship.globalFilterSection.checkFilterFieldChip(
       'Transport Mode',
       'AIR'
     );
-    const text1 = await ship.globalFilterSection.checkFilterFieldText(
+    const dateText = await ship.globalFilterSection.checkFilterFieldText(
       'Date Shipment Created',
-      'Is between 30 Days Ago and Today'
+      'Last 30 Days'
     );
-    const text2 = await ship.globalFilterSection.checkFilterFieldText(
+    const unitText = await ship.globalFilterSection.checkFilterFieldText(
       'Shipment Weight',
       '100 KG'
     );
     await expect.soft(weight).toBe(true);
     await expect.soft(volume).toBe(true);
     await expect.soft(currency).toBe(true);
+
     await expect.soft(bool).toBe(true);
-    await expect.soft(multText1).toBe(true);
-    await expect.soft(multText2).toBe(true);
-    await expect.soft(text1).toBe(true);
-    await expect.soft(text2).toBe(true);
+    await expect.soft(seaChip).toBe(true);
+    await expect.soft(airChip).toBe(true);
+    await expect.soft(dateText).toBe(true);
+    await expect.soft(unitText).toBe(true);
   });
 
   test('[21.4] User edits a dashboard and refreshes the page', async () => {
@@ -64,7 +67,6 @@ test.describe('[21] Load dashboard and filter settings', () => {
     await ship.globalFilterSection.setBasicTextFilter('Branch', 'NYC', 'MNL');
     await page.reload();
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
-    await ship.globalFilterSection.fieldBranch.click();
     const nyc = await ship.globalFilterSection.checkFilterFieldChip(
       'Branch',
       'NYC'
@@ -78,15 +80,16 @@ test.describe('[21] Load dashboard and filter settings', () => {
   });
 
   test('[21.5] User clicks on back button', async () => {
-    const ship = new ExploreShipments(page);
+    const pay = new ExplorePayableInvoices(page);
+    await pay.goto();
+    await pay.globalFilterSection.setBasicTextFilter('Branch', 'NYC', 'MNL');
     await page.goBack();
     await waitForFilterSectionToLoad(page, DEFAULT_TIMEOUT_IN_MS);
-    await ship.globalFilterSection.fieldBranch.click();
-    const nyc = await ship.globalFilterSection.checkFilterFieldChip(
+    const nyc = await pay.globalFilterSection.checkFilterFieldChip(
       'Branch',
       'NYC'
     );
-    const mnl = await ship.globalFilterSection.checkFilterFieldChip(
+    const mnl = await pay.globalFilterSection.checkFilterFieldChip(
       'Branch',
       'MNL'
     );
@@ -97,10 +100,12 @@ test.describe('[21] Load dashboard and filter settings', () => {
   test('[21.6] User edits a dashboard and shares the URL with another user from the same company', async () => {
     const ship = new ExploreShipments(page);
     const side = new SideMenu(page);
+    await ship.goto();
     const url = await page.url();
     const browser = await chromium.launch();
     const context = await browser.newContext();
     const page1 = await context.newPage();
+
     await logInAuth(
       page1,
       `${process.env.FREIGHT_BI_CLIENT2_USER}`,
@@ -118,20 +123,19 @@ test.describe('[21] Load dashboard and filter settings', () => {
       'AP Is Posted',
       'True'
     );
-    await ship.globalFilterSection.fieldTransportMode.click();
-    const multText1 = await ship.globalFilterSection.checkFilterFieldChip(
+    const seaChip = await ship.globalFilterSection.checkFilterFieldChip(
       'Transport Mode',
       'SEA'
     );
-    const multText2 = await ship.globalFilterSection.checkFilterFieldChip(
+    const airChip = await ship.globalFilterSection.checkFilterFieldChip(
       'Transport Mode',
       'AIR'
     );
-    const text1 = await ship.globalFilterSection.checkFilterFieldText(
+    const dateText = await ship.globalFilterSection.checkFilterFieldText(
       'Date Shipment Created',
-      'Is between 30 Days Ago and Today'
+      'Last 30 Days'
     );
-    const text2 = await ship.globalFilterSection.checkFilterFieldText(
+    const unitText = await ship.globalFilterSection.checkFilterFieldText(
       'Shipment Weight',
       '100 KG'
     );
@@ -139,10 +143,10 @@ test.describe('[21] Load dashboard and filter settings', () => {
     await expect.soft(volume).toBe(true);
     await expect.soft(currency).toBe(true);
     await expect.soft(bool).toBe(true);
-    await expect.soft(multText1).toBe(true);
-    await expect.soft(multText2).toBe(true);
-    await expect.soft(text1).toBe(true);
-    await expect.soft(text2).toBe(true);
+    await expect.soft(seaChip).toBe(true);
+    await expect.soft(airChip).toBe(true);
+    await expect.soft(dateText).toBe(true);
+    await expect.soft(unitText).toBe(true);
     await expect.soft(side.apiSummary.getByText('CN0TRNMNL')).toBeVisible();
     await expect.soft(side.apiSummary.getByText('+2')).toBeVisible();
   });
