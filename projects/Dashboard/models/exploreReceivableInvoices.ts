dc6f517a148bed5af31168fc5d26b538d4e3a703
mai-expedock <imma.damalerio@expedock.com>
2025-02-18 23:33:20 +1100

    changes to 47 49 51 53 and 57

--- projects/Dashboard/models/exploreReceivableInvoices.ts
+++ projects/Dashboard/models/exploreReceivableInvoices.ts
@@ -2,16 +2,19 @@ import { Locator, Page } from '@playwright/test';
 import { waitforTablePageLoad } from '../../utils';
 import { GlobalNativeTable } from './globalNativeTable';
 import { DEFAULT_TIMEOUT_IN_MS, FREIGHT_BI_BASE_URL } from '../../constants';
+import { GlobalFilterSection } from './globalFilterSection';
 
 export class ExploreReceivableInvoices {
   readonly page: Page;
   readonly globalNativeTable: GlobalNativeTable;
+  readonly globalFilterSection: GlobalFilterSection;
   readonly referenceComponent: Locator;
   readonly columnInvoiceNumber: Locator;
 
   constructor(page: Page) {
     this.page = page;
     this.globalNativeTable = new GlobalNativeTable(page);
+    this.globalFilterSection = new GlobalFilterSection(page);
     this.referenceComponent = page.getByTestId('invoice_number').first();
     this.columnInvoiceNumber = page.getByTestId('table-header-invoice_number');
   }
